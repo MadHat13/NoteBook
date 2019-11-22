447ad2b0ea63865510f6b6047e7b08de03d655ff
MadHat13 <1124298082@qq.com>
2019-11-22 14:12:37 +0800

    1

---
+++ notebook/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <router-link to="/"></router-link>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Head from './components/Head.vue';
+
+@Component({
+  components: {
+    Head,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ notebook/src/views/Login.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <form id="text11" method="get">
+      年纪
+      <input type="number" id="number1" max="200" min="1" required="required" name="age" />
+      <br />姓名
+      <input type="text" id="text1" required="required" name="name" />
+      <button type="submit" onclick="test()">提交</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import {} from '../assets/js/Login';
+
+export default {
+  methods: {
+    test() {
+      const age = document.getElementById('number1');
+      const name = document.getElementById('text1');
+      const jsondata = {
+        age: age.value,
+        name: name.value,
+      };
+      console.log(age.value);
+      console.log(name.value);
+      $.ajax({
+        type: 'post',
+        url: 'login',
+        data: jsondata,
+        success: function(data) {
+          alert('前端请求后端成功，返回了数据,数据显示用户名为' + data.name);
+        },
+        error: function(e) {
+          alert('发生未知错误');
+        }
+      });
+    },
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
